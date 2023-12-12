6db61651eba6ee565669cba1d4aaf9fd556a636d
BrianEBranch <brianramos892@gmail.com>
2023-12-12 12:39:13 -0500

    added hashing examples

--- Hashing/src/Main.java
+++ Hashing/src/Main.java
@@ -1,5 +1,11 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Map<Integer, String> exHashMap = new HashMap<>();
+        Set<Integer> exHashSet = new HashSet<>();
     }
 }
